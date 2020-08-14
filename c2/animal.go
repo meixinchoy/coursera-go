e7958b556928f77f430ff5e7fb30314357017d10
meixin <meixinchoy@gmail.com>
2020-08-15 00:17:53 +0800

    added comments

--- c2/animal.go
+++ c2/animal.go
@@ -34,6 +34,8 @@ func (a Animal) Speak(){
 	fmt.Println(a.sound)
 }
 
+
+
 func main(){
 	var animals[3] Animal
 	animals[0].SetAttributes("cow","grass","walk","moo")
@@ -44,32 +46,32 @@ func main(){
 	var err error
 
 	for {
-		var checking bool = false
+		var checking bool = false		
 		fmt.Print("\n>")
 		request, err = reader.ReadString('\n')
 		request= strings.Trim(request,"\n\r")
-		requestArr:= strings.Split(request, " ")
+		requestArr:= strings.Split(request, " ")  //split str into arr
 		
 
 		if err == nil {
 			for i:=0; i<3; i++ {
-				if requestArr[0]==animals[i].Name(){
+				if requestArr[0]==animals[i].Name(){		//correct animal name
 					if requestArr[1]=="eat"{
-						animals[i].Eat()
+						animals[i].Eat()					//eat ins
 					}else if requestArr[1]=="speak"{
-						animals[i].Speak()
+						animals[i].Speak()					//speak ins
 					}else if requestArr[1]=="move"{
-						animals[i].Move()
+						animals[i].Move()					//move ins
 					}else{
 						checking=false
-						break;
+						break;				// incorrect ins, break from loop
 					}
-					checking=true
+					checking=true			// ins ald executed, break from loop
 					break
 				}
-			}
+			}		//checking false by default (if wrong animal name is entered)
 		}
-		if checking== false{
+		if checking== false{	 // display err msg if user entered incorrect input
 			fmt.Print("Please enter valid input.\n")
 		}
 	}
