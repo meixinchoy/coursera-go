70dd037dc3842466d0bf6ac7e609febf8127a574
meixin <meixinchoy@gmail.com>
2020-09-02 21:32:17 +0800

    added peer ans for racecondition

--- c3/racecondition.go
+++ c3/racecondition.go
@@ -27,3 +27,32 @@ func main() {
 }
 
 /*Race condition is a behavior which occurs where the output depends on non-deterministic ordering. In other words, it is dependent on the sequence of the events. Race condition occur due to the change of sequence when communicating between tasks*/
+
+/*
+ code from peer evaluation (by Amsavarthan Lv):
+
+package main
+
+import (
+  "fmt"
+)
+
+func add_one(pt *int) {
+  (*pt)++
+  fmt.Println(*pt)
+}
+
+func sub_one(pt *int) {
+  (*pt)--
+  fmt.Println(*pt)
+}
+func main(){
+  i := 0
+  go add_one(&i)
+  go sub_one(&i)
+
+  i++
+  fmt.Println()
+
+}
+*/
